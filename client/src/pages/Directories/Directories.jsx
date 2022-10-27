30808486b16eb80f18b86bfe30c4db98ccb0f322
daliondev <d4lion.dev@gmail.com>
2022-10-27 11:31:12 -0500

    :lipstick:  update the UI

--- client/src/pages/Directories/Directories.jsx
+++ client/src/pages/Directories/Directories.jsx
@@ -1,4 +1,5 @@
 import DirectoryButton from "./components/DirectoryButton"
+import { AiOutlinePlus } from "react-icons/ai"
 
 const DirectoriesPage = () => {
   return (
@@ -8,6 +9,9 @@ const DirectoriesPage = () => {
       </h1>
       <div className="grid grid-cols-1   sm:grid-cols-4 gap-4 px-16">
         <DirectoryButton />
+        <button className="flex justify-center   bg-slate-700 hover:bg-slate-500 p-4 rounded-md text-slate-200">
+          <AiOutlinePlus className="h-8" />
+        </button>
       </div>
     </>
   )
