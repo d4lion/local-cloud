136ce9ec2e933535a3947c190a6f25a5323fee7e
daliondev <d4lion.dev@gmail.com>
2022-11-07 19:14:28 -0500

    :sparkles: Added feature to create a dir

--- server/src/routes/createDir.js
+++ server/src/routes/createDir.js
@@ -4,11 +4,17 @@ const { cwd } = require("process")
 
 const router = Router()
 
-router.put("/create/:dir", (req, res) => {
-  if (fs.existsSync(`${cwd()}/server/upload/${req.params.dir}`)) {
-    res.send("Existe")
+router.put("/create/dir/:dir", (req, res) => {
+  if (!fs.existsSync(`${cwd()}/server/upload/${req.params.dir}`)) {
+    fs.mkdir(`${cwd()}/server/upload/${req.params.dir}`, (err) => {
+      if (err) {
+        res.status(500).send(err)
+      } else {
+        res.status(200).json({ exist: false, name: req.params.dir })
+      }
+    })
   } else {
-    res.send("No existe")
+    res.status(200).json({ exist: true })
   }
 })
 
